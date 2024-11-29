0106037c3fda77fcdbad3ad2280a52e03372011a
mur4ik18 <o.o.kotov1999@gmail.com>
2024-11-29 15:14:33 +0100

    Add test

--- src/test/java/org/kotov/tp1/AppTest.java
+++ src/test/java/org/kotov/tp1/AppTest.java
@@ -1,4 +1,25 @@
 package org.kotov.tp1;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
 public class AppTest {
+
+    @Test
+    public void Test() {
+        Zoo zoo = new Zoo();
+        zoo.ajouterSecteur(TypeAnimal.CHAT);
+        Exception exc = null;
+        try {
+            zoo.nouveauVisiteur();
+            zoo.nouveauVisiteur();
+        } catch (Exception e) {
+            exc = e;
+        }
+        assertNull(exc);
+
+        System.out.println("Test 1 " + exc);
+
+    }
 }
